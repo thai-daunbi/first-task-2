2d54d409f3a563073d6a19f12ce8009f90d2ab47
leo.daunbi <leothai.daunbi@gmail.com>
2023-03-11 10:39:32 +0700

    routes(not)

--- images/php/app/routes/web.php
+++ images/php/app/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
@@ -12,7 +13,19 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
+echo "web.php";
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/', 'UserController@index');
+
+// Route::domain('{subdomain}.example.com')->group(function () {
+//     Route::prefix('')->group(function () {
+//         Route::get('getUser/{id}', 'sampleController@showGet')->where('id', '[0-9]+');
+//         Route::post('getUser', 'sampleController@showPost');
+//     });
+// });
